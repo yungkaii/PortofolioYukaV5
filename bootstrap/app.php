e5d07eeb17f075b34f76309c2fdf8f01231afd4e
unknown <vsgabcd48@gmail.com>
2026-08-19 17:46:58 +0700

    restore Laravel bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,21 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-    $exceptions->shouldRenderJsonWhen(
-        fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-    );
-
-    $exceptions->render(function (Throwable $e, Request $request) {
-        return response(
-            '<h1>Laravel diagnostic</h1><pre>'
-            . e(get_class($e))
-            . "\n\n"
-            . e($e->getMessage())
-            . "\n\n"
-            . e($e->getFile() . ':' . $e->getLine())
-            . '</pre>',
-            500,
-            ['Content-Type' => 'text/html; charset=UTF-8']
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
-    });
-})
+    })
+    ->create();
