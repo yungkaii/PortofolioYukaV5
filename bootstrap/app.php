81695f56c05d07b3be677fce75387c7e9fb52254
unknown <vsgabcd48@gmail.com>
2026-08-19 17:50:09 +0700

    register view service provider for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\View\ViewServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        ViewServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
